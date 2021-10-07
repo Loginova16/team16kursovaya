9ddfbc6c1f191e81938b9a23a730a13272a446db
Khairullina <eliina.kh@mail.ru>
2021-10-08 02:33:12 +0500

    Edit

---
+++ src/test/java/Calcul/CalculateTest.java
@@ -0,0 +1,26 @@
+/*
+package Calcul;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculateTest {
+
+	Calculate calc;
+	@Before
+    public void setUp() throws Exception {
+		calc = new Calculate("10", "6", "10000", true, true); // ñîçäàåì ýêçåìïëÿð êëàññà calculate êîòîðûé âûñ÷èòàåò èòîãîâîå çíà÷åíèå âêëàäà
+    }
+
+    @Test
+    public void test() {
+    	 System.out.println(calc.getFinalValue());
+        assertTrue(calc.getFinalValue() == 626400.0); // óáåæäàåìñÿ ÷òî ðåçóëüòàò âåðåí
+    }
+
+}
+*/
---
+++ src/test/java/Calcul/parseTest.java
@@ -0,0 +1,21 @@
+/*
+package Calcul;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class parseTest {
+	Calculate calc;
+	@Before
+    public void setUp() throws Exception {
+		calc = new Calculate("10", "6", "10000", true, true); // ñîçäàåì ýêçåìïëÿð êëàññà calculate - âõîäíûå äàííûå íå èèìåþò çíà÷åíèÿ
+    }
+
+    @Test
+    public void test() {
+        assertTrue(calc.ParseAllData("10", "0.023f", "010.2")); // óáåæäàåìñÿ ÷òî äàííûå ìîãóò áûòü êîíâåðòèðîâàííû â Float
+    }
+}
+*/
